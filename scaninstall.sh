f55fa7c9d0b9f471e306b7d06b973088d147ba1a
txsouth <duelmasters.darren@gmail.com>
2017-06-14 15:47:44 +0800

    more edits

--- scaninstall.sh
+++ scaninstall.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-apt-get --assume-yes install git gcc make libpcap-dev
-mkdir temporary
-cd temporary
+
+#docker
+wget -qO- https://get.docker.com/ | sh
+
+#masscan
+mkdir HOSTTools
+cd HOSTTools
 git clone https://github.com/robertdavidgraham/masscan
 cd masscan
-make -j
+make
 make regress
-mv bin/masscan /usr/bin/
-apt-get --assume-yes install nmap
+mv bin/masscan /usr/bin
+
+# nmap
+apt-get install nmap -y
+
+# create directory for docker images
+cd ~/HOSTTools
+mkdir dockerimages
+cd dockerimages
+
+# cowrie docker file
+docker pull ouspg/cowrie
+
+# kippo docker file
+docker pull tomdesinto/kippo
+
