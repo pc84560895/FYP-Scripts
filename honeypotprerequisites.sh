a8ec2087cd5f3ee2ff6f339b3a325c880f6d5628
txsouth <duelmasters.darren@gmail.com>
2017-07-03 23:17:02 +0800

    edited prerequisites

--- honeypotprerequisites.sh
+++ honeypotprerequisites.sh
@@ -7,11 +7,18 @@ wget -qO- https://get.docker.com/ | sh
 mkdir ~/HOSTTools
 cd ~/HOSTTools
 
+#create honeypot directory
+mkdir honeypots
+cd honeypots
+
+#create database for filtered logs
+cp ~/FYP-Scripts/honeypotlogsdb.py .
+python honeypotlogsdb.py
+rm honeypotlogsdb.py
+
 # cowrie docker file
 docker pull ouspg/cowrie
 
 # kippo docker file
 docker pull tomdesinto/kippo
 
-# create database for filtered logs
-python ~/FYP-Scripts/honeypotlogsdb.py
