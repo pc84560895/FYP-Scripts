d6b7728c977c7dbb7a175ff5b13724e83d55fbea
txsouth <duelmasters.darren@gmail.com>
2017-07-13 23:08:30 +0800

    added dionaeasqlitefilter with splunkdb config for dionaea and honeypy
    REMOVE honeypotlogsdb

---
+++ dionaeasqlitefilter.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import os
+import datetime
+import sqlite3
+import requests
+
+#Hard coded for now
+sqliteDB = "/var/lib/docker/volumes/Deployment_6/_data/dionaea.sqlite"
+log_file = "/var/lib/docker/volumes/Deployment_6/_data/dionaeaconn.log"
+last_conn = "/var/lib/docker/volumes/Deployment_6/_data/lastconn.id"
+
+#sqliteDB = "dionaea.sqlite"
+#log_file = "dionaeaconn.log"
+#last_conn = "lastconn.id"
+
+conn_start = 0
+conn_id = 0
+
+#log_file2 = "dionaeaoffers.log"
+#last_offer = "lastoffer.id"
+
+log_file2 = "/var/lib/docker/volumes/Deployment_6/_data/dionaeaoffers.log"
+last_offer = "/var/lib/docker/volumes/Deployment_6/_data/lastoffer.id"
+
+offer_start = 0
+offer_id = 0
+
+l_ip = requests.get("http://ipinfo.io").json().get('ip')
+
+if os.path.isfile(sqliteDB):
+
+#Start of extracting data out of connections table
+    if os.path.isfile(last_conn):
+        with open(last_conn, 'r') as f:
+            line = int(f.read())
+            if line > 0:
+                conn_start = line
+
+#Connections table
+    conn = sqlite3.connect(sqliteDB)
+    cursor = conn.cursor()
+    sqlstmt = "SELECT * FROM connections WHERE connection > {} ORDER BY connection ASC".format(conn_start)
+    cursor.execute(sqlstmt)
+
+    for i,row in enumerate(cursor):
+        timestamp = datetime.datetime.fromtimestamp(row[4]).strftime('%Y-%m-%d %H:%M:%S')
+        conn_id = row[0]
+        connection_type = row[1]
+        protocol = row[2]
+        connection_protocol = row[3]
+        l_port = row[8]
+        r_ip = row[9][7:]
+        r_port = row[11]
+        hostname = row[10]
+        with open(log_file, "a+") as f:
+            f.write("{} : {:10} \t {:10} \t {} \t {} \t {} \t {:15} \t {:5} \t {}\n".format(timestamp, connection_type, connection_protocol, protocol, l_ip, l_port, r_ip, r_port, hostname))
+
+    conn.close()
+
+    if conn_id > 0:
+        with open(last_conn, 'w+') as f:
+            f.write(str(conn_id))
+#End of Connections table
+
+
+#Start of extracting data out of offers table
+    if os.path.isfile(last_offer):
+        with open(last_offer, 'r') as f:
+            line = int(f.read())
+            if line > 0:
+                offer_start = line
+
+    conn = sqlite3.connect(sqliteDB)
+    cursor = conn.cursor()
+    sqlstmt = "SELECT offer, o.connection, offer_url, connection_timestamp FROM offers o, connections c WHERE c.connection = o.connection AND offer > {} ORDER BY offer ASC".format(offer_start)
+    cursor.execute(sqlstmt)
+
+    for i,row in enumerate(cursor):
+        timestamp = datetime.datetime.fromtimestamp(row[3]).strftime('%Y-%m-%d %H:%M:%S')
+        offer_id = row[0]
+        offer_url = row[2]
+        with open(log_file2, "a+") as f:
+            f.write("{} : {}\n".format(timestamp, offer_url))
+    conn.close()
+
+    if offer_id > 0:
+        with open(last_offer, 'w+') as f:
+            f.write(str(offer_id))
+
+else:
+    print("Sqlite DB not found: {}".format(sqliteDB))
--- honeypotlogsdb.py
+++ honeypotlogsdb.py
@@ -11,7 +11,7 @@ cursor.execute("use filteredlogsdb")
 #connections table
 cursor.execute('''CREATE TABLE IF NOT EXISTS
                         connections(
-                                connection_id int NOT NULL AUTO_INCREMENT,
+                                connection_id int NOT NULL,
                                 connection_transport varchar(255),
                                 connection_protocol varchar(255),
                                 connection_timestamp timestamp,
@@ -25,7 +25,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS
 #Downloads table
 cursor.execute('''CREATE TABLE IF NOT EXISTS 
 			downloads (
-				download_id int NOT NULL AUTO_INCREMENT,
+				download_id int NOT NULL,
 				connection int,
 				download_url varchar(1000),
 				download_md5_hash varchar(500),
@@ -33,46 +33,57 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS
 				CONSTRAINT downloads_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
 			)''')
 
-#sqllogins table
-cursor.execute('''CREATE TABLE IF NOT EXISTS
-			sqllogins (
-				login_id int AUTO_INCREMENT,
-				connection int,
-				login_username varchar(1000),
-				login_password varchar(1000),
-				PRIMARY KEY(login_id),
-				CONSTRAINT logins_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
+
+
+#offer table for dl links if there is one
+cursor.execute('''CREATE TABLE IF NOT EXISTS 
+			offers (
+				offer INTEGER PRIMARY KEY,
+				connection INTEGER,
+				offer_url TEXT
+				-- CONSTRAINT offers_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection)
 			)''')
 
-#mssql_commands table
-cursor.execute('''CREATE TABLE IF NOT EXISTS
-			mssql_commands (
-				mssql_command_id int AUTO_INCREMENT,
-				connection int,
-				mssql_command_status varchar(1000),
-				mssql_command_cmd varchar(5000),
-				PRIMARY KEY(mssql_command_id),
-				CONSTRAINT mssql_commands_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
-			)''')
-
-#mssql_fingerprints table
-cursor.execute('''CREATE TABLE IF NOT EXISTS
-			mssql_fingerprints (
-				mssql_fingerprint_id int AUTO_INCREMENT,
-				connection int,
-				mssql_fingerprint_hostname varchar(1000),
-				mssql_fingerprint_appname varchar(1000),
-				mssql_fingerprint_cltintname varchar(1000),
-				PRIMARY KEY(mssql_fingerprint_id),
-				CONSTRAINT mssql_fingerprints_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
-			)''')
+# #sqllogins table
+# cursor.execute('''CREATE TABLE IF NOT EXISTS
+			# sqllogins (
+				# login_id int AUTO_INCREMENT,
+				# connection int,
+				# login_username varchar(1000),
+				# login_password varchar(1000),
+				# PRIMARY KEY(login_id),
+				# CONSTRAINT logins_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
+			# )''')
 			
-#mysql_command_args table
-cursor.execute('''CREATE TABLE IF NOT EXISTS
-			mysql_command_args (
-				mysql_command_arg_id int AUTO_INCREMENT,
-				mysql_command int,
-				mysql_command_arg_index int NOT NULL,
-				mysql_command_arg_data varchar(5000) NOT NULL,
-				PRIMARY KEY(mysql_command_arg_id),
-			)''')			
+# #mssql_commands table
+# cursor.execute('''CREATE TABLE IF NOT EXISTS
+			# mssql_commands (
+				# mssql_command_id int AUTO_INCREMENT,
+				# connection int,
+				# mssql_command_status varchar(1000),
+				# mssql_command_cmd varchar(5000),
+				# PRIMARY KEY(mssql_command_id),
+				# CONSTRAINT mssql_commands_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
+			# )''')
+
+# #mssql_fingerprints table
+# cursor.execute('''CREATE TABLE IF NOT EXISTS
+			# mssql_fingerprints (
+				# mssql_fingerprint_id int AUTO_INCREMENT,
+				# connection int,
+				# mssql_fingerprint_hostname varchar(1000),
+				# mssql_fingerprint_appname varchar(1000),
+				# mssql_fingerprint_cltintname varchar(1000),
+				# PRIMARY KEY(mssql_fingerprint_id),
+				# CONSTRAINT mssql_fingerprints_connection_fkey FOREIGN KEY (connection) REFERENCES connections (connection_id)
+			# )''')
+			
+# #mysql_command_args table
+# cursor.execute('''CREATE TABLE IF NOT EXISTS
+			# mysql_command_args (
+				# mysql_command_arg_id int AUTO_INCREMENT,
+				# mysql_command int,
+				# mysql_command_arg_index int NOT NULL,
+				# mysql_command_arg_data varchar(5000) NOT NULL,
+				# PRIMARY KEY(mysql_command_arg_id),
+			# )''')			
