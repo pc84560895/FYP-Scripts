23f858edd9bdcab373f8e89977c80190ee676536
txsouth <duelmasters.darren@gmail.com>
2017-08-10 14:09:59 +0800

    cleaning

--- honeypotprerequisites.sh
+++ honeypotprerequisites.sh
@@ -7,24 +7,6 @@ wget -qO- https://get.docker.com/ | sh
 mkdir ~/HOSTTools
 cd ~/HOSTTools
 
-#create honeypot directory
-#mkdir Honeypots
-#cd Honeypots
-
-#create database for filtered logs
-#cp ~/FYP-Scripts/honeypotlogsdb.py .
-#python honeypotlogsdb.py
-#rm honeypotlogsdb.py
-
-#Install MySQL-python
-#pip install MySQL-python
-
-# cowrie docker file
-#docker pull ouspg/cowrie
-
-# kippo docker file
-#docker pull tomdesinto/kippo
-
 #Install Requests
 pip install requests
 
